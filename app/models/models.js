e8cfb6d71ce3360f75482ed6707ae0c95731e67e
hanshavin <hansevangelista@gmail.com>
2014-05-31 00:11:28 -0500

    Revert "este es un nuevo commit"
    This reverts commit f4a4faec4a7cd210725ff7c00fe12c262409f71a.

--- app/models/models.js
+++ app/models/models.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose')
 
-var uristring = process.env.MONGOLAB_URI ||
-				process.env.MONGOHQ_URL ||
-				'mongodb://localhost/amtrepdb';
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/amtrepdb';
 
 mongoose.connect(uristring)
 
