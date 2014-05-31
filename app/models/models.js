f4a4faec4a7cd210725ff7c00fe12c262409f71a
hanshavin <hansevangelista@gmail.com>
2014-05-31 00:08:01 -0500

    este es un nuevo commit

--- app/models/models.js
+++ app/models/models.js
@@ -1,9 +1,8 @@
 var mongoose = require('mongoose')
 
-var uristring =
-process.env.MONGOLAB_URI ||
-process.env.MONGOHQ_URL ||
-'mongodb://localhost/amtrepdb';
+var uristring = process.env.MONGOLAB_URI ||
+				process.env.MONGOHQ_URL ||
+				'mongodb://localhost/amtrepdb';
 
 mongoose.connect(uristring)
 
